044a08dde365a1458e7c41c67440fa4af6e3c05f
Ayush Chauhan <ayushdchauhan2004@gmail.com>
2026-08-20 15:09:42 +0930

    added recording functionality to page and stop button with audio player

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,82 @@
+let mediaRecorder;
+
+let recordButton = document.getElementById('record-button');
+
+let stopButton = document.getElementById('stop-button');
+
+let recordedChunks = []
+
+// for audio player
+const audioPlayer = document.getElementById('recorded-audio');
+
+// clicking record button
+recordButton.addEventListener('click', () => {
+		// test: console.log("Hello")
+		
+		async function requestMicAccess() {
+			try {
+				// audio permission request
+				const stream = await navigator.mediaDevices.getUserMedia({ audio:true });
+				console.log("access for mic granted")
+				mediaRecorder = new MediaRecorder(stream);
+				
+				// collecting data chunks
+				mediaRecorder.ondataavailable = (event) => {
+					if (event.data.size > 0) {
+						recordedChunks.push(event.data);
+					} 
+				};
+				
+				mediaRecorder.onstop = () => {
+					console.log('Recording Finished')
+					const blob = new Blob(recordedChunks, { type: mediaRecorder.mimeType });
+					console.log(mediaRecorder.mimeType);
+					const recordingURL = URL.createObjectURL(blob);
+					audioPlayer.src = recordingURL;
+					
+					
+					
+					console.log('blob size:', blob.size);
+					console.log('blob type:', blob.type);
+					console.log('player src:', audioPlayer.src);
+				}
+				
+				// clear chunks for next session
+				recordedChunks = [];
+				mediaRecorder.start();
+				
+				recordButton.disabled = true
+				stopButton.disabled = false
+				
+				recordButton.innerHTML = 'Recording';
+				// testing if button disables
+				console.log(recordButton.disabled);
+				console.log(stopButton.disabled);
+			} catch (error) {
+				// if user denies or no mic found
+				if (error.name === 'NotAllowedError' || error.name === 'PermissionDeniedError') {
+					console.error("user denied access")
+				} else if (error.name === 'NotFoundError' || error.name === 'DeviceNotFoundError') {
+					console.error('no mic device found on system');
+				} else {
+					console.error('mic error', error);
+				}
+			}
+		}
+		requestMicAccess();
+	})
+	
+// stop button
+stopButton.addEventListener('click', () => {
+	mediaRecorder.stop()
+	
+	recordButton.disabled = false
+	stopButton.disabled = true
+	
+	// testing if button disables
+	console.log(recordButton.disabled);
+	console.log(stopButton.disabled);
+	
+	recordButton.innerHTML = 'Record';
+	
+})
